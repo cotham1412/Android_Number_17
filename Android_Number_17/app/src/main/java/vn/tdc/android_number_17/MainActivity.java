9d0ece7633c4bdf4919f9572e7188c4e4780ccdf
cotham1412 <20211tt1579@mail.tdc.edu.vn>
2022-06-01 11:14:00 +0700

    up num 2/1

--- Android_Number_17/app/src/main/java/vn/tdc/android_number_17/MainActivity.java
+++ Android_Number_17/app/src/main/java/vn/tdc/android_number_17/MainActivity.java
@@ -2,7 +2,13 @@ package vn.tdc.android_number_17;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.android.ads.mediationtestsuite.activities.HomeActivity;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            // check ktra đã đăng nhập vào thẳng vào Home
+            public void run() {
+                if (firebaseAuth.getCurrentUser() != null) {
+                    if (firebaseAuth.getCurrentUser().getEmail().length() > 0) {
+                        startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                    }
+                } else {
+                    startActivity(new Intent(MainActivity.this, SignInActivity.class));
+                }
+
+                finish();
+            }
+        }, 3500);
     }
 }
