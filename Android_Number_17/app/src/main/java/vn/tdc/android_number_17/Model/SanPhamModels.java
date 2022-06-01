ddac354b3decd7965f43f89fa847270d22fec156
cotham1412 <20211tt1579@mail.tdc.edu.vn>
2022-06-01 11:13:24 +0700

    up number 2

---
+++ Android_Number_17/app/src/main/java/vn/tdc/android_number_17/Model/SanPhamModels.java
@@ -0,0 +1,21 @@
+package vn.tdc.android_number_17.Model;
+
+public class ProductModels {
+
+    private  String id;
+
+    private  String idsp;
+    private  String tensp;
+    private  long giatien;
+    private  String hinhanh;
+    private  String loaisp;
+    private  String mota;
+    private  long soluong;
+    private  String nhasanxuat;
+    private  long type;
+    private  String baohanh;
+    private FirebaseFirestore  db;
+    private FirebaseAuth firebaseAuth;
+    private ISanPham callback;
+
+}
