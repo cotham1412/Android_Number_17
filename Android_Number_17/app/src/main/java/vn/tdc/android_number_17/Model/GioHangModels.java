b559d10891859a31c5e18e8f04696fb10a2dacac
cotham1412 <20211tt1579@mail.tdc.edu.vn>
2022-06-01 21:02:20 +0700

    up number 5

--- Android_Number_17/app/src/main/java/vn/tdc/android_number_17/Model/GioHangModels.java
+++ Android_Number_17/app/src/main/java/vn/tdc/android_number_17/Model/GioHangModels.java
@@ -1,59 +1,59 @@
-package vn.tdc.android_number_17.Model;
-
-
-import android.util.Log;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-
-public class GioHangModels {
-    private  String id;
-    private  String id_sanpham;
-    private  long soluong;
-    private IGioHang callback;
-    private FirebaseFirestore db;
-
-    public  GioHangModels(){
-
-    }
-    public GioHangModels(String id, String id_sanpham, long soluong) {
-        this.id = id;
-        this.id_sanpham = id_sanpham;
-        this.soluong = soluong;
-    }
-
-    public GioHangModels(String id_sanpham, long soluong) {
-        this.id_sanpham = id_sanpham;
-        this.soluong = soluong;
-    }
-
-    public  GioHangModels(IGioHang callback){
-        this.callback=callback;
-        db= FirebaseFirestore.getInstance();
-
-    }
-
-    public long getSoluong() {
-        return soluong;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getId_sanpham() {
-        return id_sanpham;
-    }
-
-}
+//package vn.tdc.android_number_17.Model;
+//
+//
+//import android.util.Log;
+//import android.widget.Toast;
+//
+//import androidx.annotation.NonNull;
+//
+//import com.google.android.gms.tasks.OnCompleteListener;
+//import com.google.android.gms.tasks.OnSuccessListener;
+//import com.google.android.gms.tasks.Task;
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.firestore.DocumentReference;
+//import com.google.firebase.firestore.DocumentSnapshot;
+//import com.google.firebase.firestore.FirebaseFirestore;
+//import com.google.firebase.firestore.QueryDocumentSnapshot;
+//import com.google.firebase.firestore.QuerySnapshot;
+//
+//
+//public class GioHangModels {
+//    private  String id;
+//    private  String id_sanpham;
+//    private  long soluong;
+//    private IGioHang callback;
+//    private FirebaseFirestore db;
+//
+//    public  GioHangModels(){
+//
+//    }
+//    public GioHangModels(String id, String id_sanpham, long soluong) {
+//        this.id = id;
+//        this.id_sanpham = id_sanpham;
+//        this.soluong = soluong;
+//    }
+//
+//    public GioHangModels(String id_sanpham, long soluong) {
+//        this.id_sanpham = id_sanpham;
+//        this.soluong = soluong;
+//    }
+//
+//    public  GioHangModels(IGioHang callback){
+//        this.callback=callback;
+//        db= FirebaseFirestore.getInstance();
+//
+//    }
+//
+//    public long getSoluong() {
+//        return soluong;
+//    }
+//
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public String getId_sanpham() {
+//        return id_sanpham;
+//    }
+//
+//}
--- Android_Number_17/app/src/main/java/vn/tdc/android_number_17/Model/UserModel.java
+++ Android_Number_17/app/src/main/java/vn/tdc/android_number_17/Model/UserModel.java
@@ -1,27 +1,27 @@
-package vn.tdc.android_number_17.Model;
-
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuth;
-public class UserModel {
-    private FirebaseAuth firebaseAuth;
-    private  String valid_email="[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
-    private  String email;
-    private  String pass;
-    private IUSER callback;
-
-    public  UserModel(){
-
-    }
-    //dùng interface callback đến Iuser Presenter
-    public  UserModel(IUSER callback){
-        this.callback=callback;
-        firebaseAuth = FirebaseAuth.getInstance();
-
-    }
-
-}
+//package vn.tdc.android_number_17.Model;
+//
+//import androidx.annotation.NonNull;
+//
+//import com.google.android.gms.tasks.OnCompleteListener;
+//import com.google.android.gms.tasks.Task;
+//import com.google.firebase.auth.AuthResult;
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.FirebaseAuth;
+//public class UserModel {
+//    private FirebaseAuth firebaseAuth;
+//    private  String valid_email="[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+//    private  String email;
+//    private  String pass;
+//    private IUSER callback;
+//
+//    public  UserModel(){
+//
+//    }
+//    //dùng interface callback đến Iuser Presenter
+//    public  UserModel(IUSER callback){
+//        this.callback=callback;
+//        firebaseAuth = FirebaseAuth.getInstance();
+//
+//    }
+//
+//}
