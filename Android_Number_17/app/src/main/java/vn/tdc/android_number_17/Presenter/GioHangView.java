ffa779877a92bdc8b73bdc74eacdd711c369f612
cotham1412 <20211tt1579@mail.tdc.edu.vn>
2022-06-03 22:04:26 +0700

    up xx

--- Android_Number_17/app/src/main/java/vn/tdc/android_number_17/Presenter/GioHangView.java
+++
@@ -1,9 +0,0 @@
-package vn.tdc.android_number_17.Presenter;
-
-public interface GioHangView {
-    void OnSucess();
-
-    void OnFail();
-
-    void getDataSanPham(String id, String idsp,String tensp, Long giatien, String hinhanh, String loaisp, Long soluong, String nhasanxuat, Long type, String baohanh);
-}
--- Android_Number_17/app/src/main/java/vn/tdc/android_number_17/Presenter/SetOnItemClick.java
+++
@@ -1,8 +0,0 @@
-package vn.tdc.android_number_17.Presenter;
-
-import android.view.View;
-
-public interface SetOnItemClick {
-
-    void  SetItemClick(View view, int pos);
-}
